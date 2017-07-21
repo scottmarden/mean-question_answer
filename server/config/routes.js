3393f99b086466192a17e156d76e06bd1fae2d98
Scott <marden.scott@gmail.com>
2017-07-20 22:03:15 -0700

    Initial Commit. beginning of project, no work done yet

---
+++ server/config/routes.js
@@ -0,0 +1,5 @@
+const controller = require('../controllers/controller.js');
+
+module.exports = app => {
+	// enter routes here. [Format is app.<app/get/post/delete>('url', callback)]
+}
---
+++ server/controllers/controller.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+	// enter controller methods here
+}
